30bfa03a71f5c5c8f5bb04bfa0493793679b1b2a
peter-mueller <peter7995@gmail.com>
2016-04-02 14:32:47 +0200

    dockerhub deploy

--- travis-runner.sh
+++ travis-runner.sh
@@ -4,7 +4,10 @@ set -o pipefail
 if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]  && [ "$TRAVIS_NODE_VERSION" = "5.1" ]
 then
   npm test
-  echo "todo deploy"
+  gulp
+  docker build -t peter-mueller/flight-analyzer .
+  docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+  docker push peter-mueller/flight-analyzer
 else
   npm test
 fi
