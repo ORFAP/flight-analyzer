de379c0c9cc64e97635db104dd3f3c51a71dd0dd
Fabian Holtkötter <fabianholtkoetter@gmail.com>
2016-04-28 18:51:05 +0200

    travis gulp fix

--- travis-runner.sh
+++ travis-runner.sh
@@ -4,7 +4,7 @@ set -o pipefail
 if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]  && [ "$TRAVIS_NODE_VERSION" = "5.1" ]
 then
   npm test
-  gulp
+  gulp build
   docker build -t petermueller/flight-analyzer .
   docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
   docker push petermueller/flight-analyzer
